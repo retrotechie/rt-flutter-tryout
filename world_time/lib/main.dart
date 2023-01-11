19bd5712a0e632d4767a461ee436c2b09ed6211f
Retro Techie <retrotechie@proton.me>
2023-01-11 14:27:21 -0800

    feat: perform hot reload
    Hot reload automatically reloads the app when changes are saved.
    
    Issue #1

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(MaterialApp(home: Home()));
+}
+
+// `extends StatelessWidget` enables hot reload, whenever changes are made and
+// saved, the app will auto reload in the preview screen. Snippet `stless`
+//
+// Stateless widgets -> the state of the widget CANNOT change over time e.g.
+// layout, colors, final data
+// Stateful widgets -> the state of the widget CAN change over time
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("World Time"),
         centerTitle: true,
@@ -10,7 +23,7 @@ void main() {
       ),
       body: Center(
         child: Text(
-          "Retro Techie",
+          "Retro Techie hot reload",
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
@@ -25,6 +38,6 @@ void main() {
         child: Text('click'),
         backgroundColor: Colors.orange[600],
       ),
-    ),
-  ));
+    );
+  }
 }
